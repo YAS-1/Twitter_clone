361debe3ba2f24f9cac700cb0a344786109d937c
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2025-01-05 08:33:32 +0300

    backend setup

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -6,13 +6,13 @@ import { signup, login, logout } from "../controllers/auth.controllers.js";
 const authRoutes = express.Router();
 
 
-authRoutes.post("/signup", signup);
+authRoutes.post("/signup", signup); // The signup endpoint, which is a POST request that takes two parameters ie the path (/signup) and the request handler function(signup - imported from the auth.controllers.js file)
 
 
-authRoutes.get("/login", login);
+authRoutes.post("/login", login); // The login endpoint, which is a POST request that takes two parameters ie the path (/login) and the request handler function(login - imported from the auth.controllers.js file)
 
 
-authRoutes.get("/logout", logout);
+authRoutes.post("/logout", logout); // The logout endpoint, which is a POST request that takes two parameters ie the path (/logout) and the request handler function(logout - imported from the auth.controllers.js file)
 
 
 export default authRoutes;
